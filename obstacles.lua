038aa44f7cbb601b7182ddf2f34411280a37347f
Mike <github@myqly.com>
2022-03-03 17:28:08 -0600

    Fix obstacle speed to match ground. Allow player to jump-cancel the slide.

--- obstacles.lua
+++ obstacles.lua
@@ -3,23 +3,24 @@
 local obstacles = {}
 local obstacle = {}
 obstacle.type = nil -- regular, tall or floating
-obstacle.speed = 20 -- Same as ground, maybe faster for flying type.
+obstacle.speed = 40 -- Same as ground, maybe faster for flying type.
 obstacle.max = 3
 obstacle.current = 0
 obstacle.x = 1280 + 200 -- subject to change
 obstacle.y = 496 -- should be different for ground obstacles
 obstacle.delay = 3 -- time which should pass between obstacles (so we don't have 2/3 right together)
+obstacle.lastType = nil -- notes what the last obstacle generated was so we won't have 2 in a row.
 
 --[[
-	[1] = {x = 1400, y = 496, w = 128, h = 128},
-	[2] = {x = 1800, y = 368, w = 128, h = 256}, --368
-	[3] = {x = 2200, y = 416, w = 128, h = 128}
+	[1] = {x = 1400, y = 496, w = 128, h = 128}, -- short
+	[2] = {x = 1800, y = 368, w = 128, h = 256}, -- tall
+	[3] = {x = 2200, y = 416, w = 128, h = 128}  -- floating
 ]]--
 
 obstacles = {
-	[1] = {x = 1400, y = 368, w = 128, h = 256},
-	[2] = {x = 1800, y = 368, w = 128, h = 256}, --368
-	[3] = {x = 2200, y = 368, w = 128, h = 256}
+	[1] = {x = 1400, y = 496, w = 128, h = 128}, -- short
+	[2] = {x = 1800, y = 368, w = 128, h = 256}, -- tall
+	[3] = {x = 2200, y = 416, w = 128, h = 128}  -- floating
 }
 
 -- table.insert(obstacles, {1480, 496, false}) -- 496 on the ground, 396 floats
