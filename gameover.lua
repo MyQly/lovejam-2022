5af74ca6ba1d4a4d26ccdc5fb44ba1d45fa87a18
Mike <github@myqly.com>
2022-03-03 20:29:29 -0600

    Added Game Over messaging and key prompts. Added Title key prompts.

--- gameover.lua
+++ gameover.lua
@@ -7,5 +7,8 @@ function DrawGameOver()
 	w = love.graphics.getWidth()
 	love.graphics.setFont(bigFont)
 	love.graphics.printf("Game Over!",0,200,w,"center")
+	love.graphics.printf("Score: "..math.floor(gameTime*100), 0, 300, w, "center")
 	love.graphics.setFont(font)
+	love.graphics.printf("Press Space To Start",0,450, w,"center")
+	love.graphics.printf("Press Escape To Quit",0,500, w,"center")
 end
--- gametitle.lua
+++ gametitle.lua
@@ -8,4 +8,6 @@ function DrawGameTitle()
 	love.graphics.setFont(bigFont)
 	love.graphics.printf("Earthquake... Run!",0,200,w,"center")
 	love.graphics.setFont(font)
+	love.graphics.printf("Press Space To Start",0,350, w,"center")
+	love.graphics.printf("Press Escape To Quit",0,400, w,"center")
 end
