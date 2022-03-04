02275268530ff922c58997d96c69c17194e24b45
Mike <github@myqly.com>
2022-03-03 21:19:04 -0600

    Updated key prompts.

--- gameover.lua
+++ gameover.lua
@@ -9,6 +9,5 @@ function DrawGameOver()
 	love.graphics.printf("Game Over!",0,200,w,"center")
 	love.graphics.printf("Score: "..math.floor(gameTime*100), 0, 300, w, "center")
 	love.graphics.setFont(font)
-	love.graphics.printf("Press Space To Start",0,450, w,"center")
-	love.graphics.printf("Press Escape To Quit",0,500, w,"center")
+	love.graphics.printf("Press Enter To Restart or Escape to Quit",0,450, w,"center")
 end
--- gametitle.lua
+++ gametitle.lua
@@ -8,6 +8,6 @@ function DrawGameTitle()
 	love.graphics.setFont(bigFont)
 	love.graphics.printf("Earthquake... Run!",0,200,w,"center")
 	love.graphics.setFont(font)
-	love.graphics.printf("Press Space To Start",0,350, w,"center")
-	love.graphics.printf("Press Escape To Quit",0,400, w,"center")
+	love.graphics.printf("Press Enter To Start or Escape to Quit",0,350, w,"center")
+	love.graphics.printf("Press Z to Dash, Space to Jump/DoubleJump, Ctrl to Ground Slam, Shift to Duck",0,400, w,"center")
 end
